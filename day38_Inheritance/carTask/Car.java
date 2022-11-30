f7fb659f20370425eabb9222abfa78de240fcdda
Marijana <marijana.makili@gmail.com>
2022-11-30 12:05:36 -0500

    day38

--- day38_Inheritance/carTask/Car.java
+++ day38_Inheritance/carTask/Car.java
@@ -7,13 +7,16 @@ public class Car {
     public int year;
     public double price;
     public String color;
+public int miles;
 
-    public Car(String brand, String model, int year, double price, String color) {
+
+    public Car(String brand, String model, int year, double price, String color, int miles) {
         this.brand = brand;
         this.model = model;
         this.year = year;
         this.price = price;
         this.color = color;
+        this.miles = miles;
     }
 
     public void start(){
