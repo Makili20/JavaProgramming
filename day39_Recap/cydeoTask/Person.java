73064d5d40cc1ecbf410f982a32b6c71271b4869
Marijana Igracki <marijana.makili@gmail.com>
2022-12-06 11:28:37 -0500

    JavaProgramming

--- day39_Recap/cydeoTask/Person.java
+++ day39_Recap/cydeoTask/Person.java
@@ -35,12 +35,9 @@ public class Person {
     }
 
     public void setGender(char gender) {
-        if(!(gender=='M' || gender=='F')){
-            System.out.println("Invalid gender");
-            System.exit(1);
         this.gender = gender;
     }
-}
+
 
     public Person(String name, int age, char gender) {
         setName(name);
