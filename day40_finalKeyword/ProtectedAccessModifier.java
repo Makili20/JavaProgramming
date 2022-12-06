564a37772b4af01d50e495a1a7f87619279246a5
Marijana Igracki <marijana.makili@gmail.com>
2022-12-06 12:17:18 -0500

    JavaProgramming

---
+++ day40_finalKeyword/ProtectedAccessModifier.java
@@ -0,0 +1,10 @@
+package day40_finalKeyword;
+
+public class ProtectedAccessModifier {
+
+    static String name1="Cydeo";//default access modifier
+    protected static String name2="WoodenSpoon";//protected
+
+
+
+}
