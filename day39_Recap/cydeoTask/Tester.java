f6f4b2dd4defc87ea5a4f9d546f76bd7e9fc1898
Marijana Igracki <marijana.makili@gmail.com>
2022-12-06 10:54:45 -0500

    JavaProgramming

---
+++ day39_Recap/cydeoTask/Tester.java
@@ -0,0 +1,23 @@
+package day39_Recap.cydeoTask;
+
+import javax.xml.namespace.QName;
+
+public class Tester extends Employee{
+    public Tester(String name, int age, char gender, int employeeID, String jobTitle, double salary) {
+        super(name, age, gender, employeeID, jobTitle, salary);
+    }
+
+    public void createTicket(){
+        System.out.println(getJobTitle()+" "+getName()+" create a ticket");
+    }
+
+
+
+
+}
+/*
+ Create a sub class of Employee named Tester
+Override the work method
+Extra methods:
+createTicket()
+ */
