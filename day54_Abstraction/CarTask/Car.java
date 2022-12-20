d4701d566511082c59015d607810ab5623ccf9ef
Marijana Igracki <marijana.makili@gmail.com>
2022-12-20 10:53:27 -0500

    JavaProgramming

---
+++ day54_Abstraction/CarTask/Car.java
@@ -0,0 +1,40 @@
+package day54_Abstraction.CarTask;
+
+public abstract class Car {//can not be final
+
+        public abstract void start();
+
+}
+ final class A {//can be final
+ }
+class B{
+        public static void main(String[] args) {
+                A obj1=new A();
+               // Car obj2=new Car();can not create an object,Object MUST bew concrete
+
+
+        }
+}
+
+
+
+
+
+
+/*
+Car: abstract
+		start();
+
+	BMW
+		start(): push start button
+
+	Tesla
+		start(): voice control
+
+	Toyota
+		start(): Insert the key
+
+	Jeep
+		start(): jump start
+
+ */
