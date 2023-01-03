079065f86465c77834b32d3a9938f32a48a4eee5
Marijana Igracki <marijana.makili@gmail.com>
2023-01-03 14:03:40 -0500

    JavaProgramming

--- day49_Collections/SetInterfacePractice.java
+++ day49_Collections/SetInterfacePractice.java
@@ -8,15 +8,21 @@ public class SetInterfacePractice {
 
 
         Set<Integer> hashSet=new HashSet<>();
-        hashSet.addAll(Arrays.asList(10,200,300,40,90));
+        hashSet.addAll(Arrays.asList(10,200,300,40,90));//random order,accept null values
         System.out.println(hashSet);
 
 
 
 
         Set<Integer> linkedHashSet=new LinkedHashSet<>();
+        linkedHashSet.addAll(Arrays.asList(10,200,300,40,90));//kkep the insertion order,accept null values
+        System.out.println(linkedHashSet);
 
         Set<Integer> treeSet=new TreeSet<>();
+        treeSet.addAll(Arrays.asList(10,200,300,40,90));//mantains the sorted(Ascending )order,does not accept null
+        System.out.println(treeSet);
+
+
     }
 
 
